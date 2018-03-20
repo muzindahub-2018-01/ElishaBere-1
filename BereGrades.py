e2c73f9d44d1485006a31a8b83484d636ffc5b59
Elisha Bere <34088550+loktioncode@users.noreply.github.com>
2018-03-20 13:02:50 +0200

    stores student marks
    this script adds ,updates and removes student marks

---
+++ BereGrades.py
@@ -0,0 +1,125 @@
+# admins dictionary has my login credentials ...pliz use them to access the programwhen you run it
+admins = { 'Ras':'Bere',
+           'Py':'6666',
+           'Emmuel':'1592'}
+studentDict = {'Jeff':[65,25,15],
+               'Elisha':[100,100,85],
+               'Bere': [52,80]}
+
+def enterNew():
+    zita = input('Student Name: ').title()
+    mark = input('Grade: ')
+
+    studentDict[zita] = mark
+    print(zita, 'Has been added ')
+    view_grades()
+
+def enterGrades():
+    print("""
+          ******************************************
+          +     ENTER STUDENT GRADE                +
+          +                                        +
+          ****************************************** """)
+    zita = input('Student Name: ').title()
+    mark = input('Grade: ')
+    mark = int(mark)
+    if zita in studentDict:
+        print('Adding Grades....')
+        studentDict[zita].append(mark)
+        view_grades()
+    elif zita not in studentDict.keys():
+        add_new = input('{} not is sytem, ADD ? Y/n?'.format(zita))
+        add_new = add_new.lower()
+        if add_new != 'n':
+            zita = input('Student Name: ').title()
+            mark = input('Grade: ')
+            studentDict[zita] = mark
+            print(zita, 'Has been added ')
+            view_grades()
+        else:
+            print()
+            print("You did not add {} to system! ".format(zita))
+            
+    else:
+        print('Student does not exist')
+
+def rmvStudent():
+    print("""
+          ******************************************
+          +     REMOVE STUDENT GRADE                +
+          +                                         +
+          ****************************************** """)
+    zita = input('Student Name: ').title()
+
+    if zita in studentDict:
+        print('Removing Student.... ', zita)
+        del studentDict[zita]
+        view_grades()
+        print()
+        print(zita, 'Has been removed!.... ')
+    else:
+        print('Student does not exist')
+
+def view_grades():
+    for k,v in studentDict.items():         #chex all students in dict
+        print()
+        print("{} has {} % ".format(k,v))
+
+
+def main():
+    
+    print("""
+             
+      Welcome to BERE School Manangement Sytem(SMS)
+
+    [1] - Enter New Student
+    [2] - Update Grades
+    [3] - Remove Student
+    [4] - View Student Grades
+    [5] - Exit
+    """)
+    action = input('What would you Like to Do?(enter a number): ')
+
+    if action == '1':
+        enterNew()
+    elif action =='2':
+        enterGrades()
+    elif action =='3':
+        rmvStudent()
+    elif action =='4':
+        view_grades()
+    elif action == '5':                 #this line exits the program (by selecting option )
+        a = input("Are you sure you want to exit? Y/n? ").lower()
+        if a == 'y':
+            exit()
+        else:
+            main()
+        
+    else:
+        print('No Valid Choice Was Chosen!')
+def login():
+    print("""
+          *************************************************
+          +       WELCOME TO MY FIRST PROJECT             +
+          + "IT STORES STUDENT GRADES using PYTHON 3"     +
+          +                                               +
+          + made  by: Elisha Bere                         +
+          *************************************************
+
+          """)
+    print()
+    login = input('Username: ').title()
+    passwd = input('Password: ')
+
+    if login in admins:                               #chex to see if login input == one in list admins
+        if admins[login]== passwd:                   #chex if login name in admin == pwd entered
+            print('Succesfully Logged in ,',login)
+            while True:
+                main()
+        elif admins[login] != passwd:
+               print('username and password is incorrect')
+            
+    elif login not in admins :
+        print('username and password is incorrect')
+    
+login()
